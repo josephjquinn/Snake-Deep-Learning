4e9a00d2cd419e49afdbe5a54b4474598a341e74
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-07-12 23:17:40 -0400

    updating output dir / removing fig print

--- util/helper.py
+++ util/helper.py
@@ -5,8 +5,6 @@ plt.ion()
 
 #Plots score over games played to graph, includes mean score.
 def plot(scores, mean_scores):
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
     plt.clf()
     plt.title('Training...')
     plt.xlabel('Number of Games')
